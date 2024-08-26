da25525a3578d299d41e87a8cdc833bc6c37eeb9
benpaj-cps <benjamin.pajunen@gordon.edu>
2024-08-26 14:19:09 -0400

    show.py: cleanup and comments

--- jes4py/show.py
+++ jes4py/show.py
@@ -1,46 +1,84 @@
 #!/usr/bin/env python3
 
-import time
+"""
+show.py - script program to implement the "show" and "repaint" functionality
+            in JES for the JES4py package
+
+Written: 2020-07-22 Jonathan Senning <jonathan.senning@gordon.edu>
+Revised: 2020-09-02 Jonathan Senning <jonathan.senning@gordon.edu>
+- received pickled picture objects rather than filename, no need to read files
+
+Revised: 2022-08-16 by Dr. Senning 
+- initial Tkinter port
+Modified 2024-08-26 Benjamin Pajunen <benjamin.pajunen@gordon.edu>
+- changed to run as a Process instance using multiprocessing module
+- moved image display updates to main thread event handler
+
+The "show()" function in JES will open a new window and display the image
+associated with the given Picture object in the window.  The window is
+static and does not provide for user interaction.  When the "repaint()"
+function is called, however, the contents of the window are refreshed so
+that any changes to the displayed Picture (the image itself or the picture's
+title) are displayed.  Thus, repaint() can be used to produce simple
+animations.
+
+The jes4py.Picture module defines the Picture class, which has show() and
+repaint() methods.  The show() method causes this script to be run in a
+subprocess and uses a pipe to send a pickled picture object to the subprocess.
+The repaint() method checks to make sure a subprocess for this picture object
+is currently running and then sends the pickled updated picture object.
+
+Implementation note: The thread portion of this program is based on the
+first example at https://wiki.wxpython.org/LongRunningTasks.
+"""
+
 import tkinter as tk
 from PIL import ImageTk
 from multiprocessing import Process
 from queue import Queue
-from threading import Thread, enumerate, current_thread
-
-def logger(message, logging=True):
-    if logging:
-        f = open('MULTITEST.log', 'a')
-        f.write(time.ctime() + ': ' + message + '\n')
-        f.close()
+from threading import Thread
 
 class ShowProcess(Process):
+    """Process wrapper for ShowApp"""
     def __init__(self, pipe):
+        """Initializer for ShowProcess class
+        
+        Parameters
+        ----------
+        pipe : multiprocessing.Connection
+            one end of a pipe which connects to the main process
+        """
         Process.__init__(self)
         self.pipe = pipe
         self.start()
 
     def run(self):
-        logger(f"run(): Process {self.pid} has started")
-        
+        """Run "show" process"""
         # Run GUI window
         self.app = ShowApp(self.pipe)
 
         # Application has finished
         del self.app
-
-        logger(f"run(): Process {self.pid} is exiting")
         exit()
     
     def exit(self):
         """Signal the process to exit"""
-        
         self.pipe.send(ShowApp.EXIT_CODE)
 
 
 class ShowApp():
+    """App class for show program
+    """
     EXIT_CODE = bytes([0])
 
     def __init__(self, pipe):
+        """Initializer for ShowApp class
+        
+        Parameters
+        ----------
+        pipe : multiprocessing.Connection
+            one end of a pipe which connects to the main process
+        """
         # Record connection to main process
         self.pipe = pipe
         
@@ -78,7 +116,7 @@ class ShowApp():
         self.root.mainloop()
 
     def listen(self):
-        logger('Show listener has started')
+        """Run listener thread"""
         
         # Wait for an image to be sent from main process
         picture = self.pipe.recv()
@@ -104,56 +142,37 @@ class ShowApp():
         # No more images to receive; signal process to exit:
         self.root.event_generate('<<ListenerExit>>', when='head')
         
-        logger(f"showImages (thread {self.listenerThread.ident}): at end of function")
 
     def showImage(self, *args):
-        logger('Getting item from image queue')      
+        """Display an image from this subprocess's queue"""
         picture = self.imageQueue.get()
 
         try:
-            logger(f'must be an image: {picture}')
             self.root.title(picture.getTitle())
             self.image = ImageTk.PhotoImage(picture.getImage())
             self.canvas.config(width=picture.getWidth(),
                                height=picture.getHeight())        
             if self.imageID is None:
-                logger(f'showing a new image: {self.image}')
                 self.imageID = self.canvas.create_image(0, 0, image=self.image, anchor=tk.NW)
             else:
-                logger(f'repainting exisiting image (id: {self.imageID})')
                 self.canvas.itemconfig(self.imageID, image=self.image)
                 self.canvas.lift(self.imageID)
         except AttributeError:
-            logger('Attribute Error encountered')
+            print("Attempted to show a non-image")
             pass
-
-    def stopBackground(self, event, thread):
-        """Handle Python exiting"""
-        logger(f"stopBackground: threading.enumerate(): {enumerate()}")
-        logger(f'stopBackground: threading.current_thread: {current_thread()}')
-        self.listenerThread.join(timeout=1)
-        
-        if self.listenerThread.is_alive():
-            raise RuntimeError(f"Listener thread for show() failed to quit")
-        elif self.listenerThread.is_alive() == False:
-            logger(f'stopBackground: Listener thread for show ({self.listenerThread}) has stopped')
-        # exit()
     
     def onWindowClosed(self):
         """Handle GUI window close event"""
-        logger('windowClosed')
-        
-        # Request exit from main process
+        # Request exit signal from main process
         self.pipe.send(self.EXIT_CODE)
     
     def onListenerExit(self, *args):
         """Handle request from listener to exit process"""
-        logger(f'onListenerExit: current thread is {current_thread()}')
-        
+        # End the app window
         self.root.destroy()
-        self.root.quit()
-        del self.root, self.canvas
 
-        logger(f"onListenerExit: threading.enumerate(): {enumerate()}")
+        # Make sure the Tcl interpreter quits
+        self.root.quit()
+        
+        del self.root, self.canvas
         del self.listenerThread
-        logger(f"onListenerExit: 2nd threading.enumerate(): {enumerate()}")
